dd326066aeff673cd4abb8a4a9be32bb21ae3637
David Inga <david.inga@icloud.com>
2024-01-26 20:43:15 +0100

    first version

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://davidsingal.github.io',
+  base: '/davidsingal',
   integrations: [tailwind({ applyBaseStyles: false }), react()],
 });
