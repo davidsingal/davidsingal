a391b427ab957cc4e79463b52738a5f17a095f5b
David Inga <david.inga@icloud.com>
2024-02-03 11:31:08 +0100

    removing base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://davidsingal.github.io',
-  base: '/davidsingal',
+  site: 'https://davidsingal.com',
   integrations: [tailwind({ applyBaseStyles: false }), react()],
 });
